a1d7a01be5e895680c2de2bd16263e6587393bd1
itsumo.help <itsumo.help@gmail.com>
2025-11-23 17:47:00 +0900

    bugfix

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,5 +2,5 @@ import type { NextRequest } from 'next/server'
 import { version } from 'os'
  
 export async function GET(_req: NextRequest) {
-  return Response.json({ name: 'lifequalitylog', version: '0.0.1' })
+  return Response.json({ name: 'lifequalitylog', version: '0.0.2' })
 }
